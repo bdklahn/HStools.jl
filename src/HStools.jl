ff014c110996d053ed5ae8e26425017e439c57f7
Brian D. Klahn <briandk@virginia.edu>
2025-04-30 13:04:31 -0400

    integrate into one function, until it's useful to separate out any of the functionality

--- src/HStools.jl
+++ src/HStools.jl
@@ -2,63 +2,48 @@ module HStools
 
 using JSON3
 
-function load_HX(;dirpath::AbstractString="./", filename::AbstractString="H6.json")
-    filepath = joinpath(dirpath, filename)
-    if !isfile(filepath)
-        error("File not found: $filepath")
-    end
-    return JSON3.read(filepath)
-end
-export load_HX
 
-function h6_id_text(hsjson3)
-    r = hsjson3.results
-    return Dict(r.id => r.text for r in r if r.aggrlevel == 6 && length(r.id) == 6)
-end
-export h6_id_text
+"""
+Roll up the text descriptions included in the levels 8 and 10 details, from the htsdata,
+into the decription of the H6 level text.
+returns a dictionary with the H6 code as key and the text as value.
+Optionally, it can output a markdown file with the H6 codes and their descriptions (default).
+"""
+function rollupH6(;indir::AbstractString="./", outdir::AbstractString="./", outputmd::Bool=true)
+    h6_filepath = joinpath(indir, "H6.json")
+    @assert isfile(h6_filepath) "H6.json not found in $indir"
+    htsdata_filepath = joinpath(indir, "htsdata.json")
+    @assert isfile(htsdata_filepath) "htsdata.json not found in $indir"
 
-function rollup(;h6_id_text::Dict, htsdata_json3::JSON3.Array)
-    for r in htsdata_json3
+    h6_dict = Dict(r.id => r.text for r in JSON3.read(h6_filepath).results if r.aggrlevel == 6 && length(r.id) == 6)
+    for r in JSON3.read(htsdata_filepath)
         htsno_parts = split(r.htsno, ".")
         if length(htsno_parts) < 3
             continue
         end
         H6_id = join(htsno_parts[1:2])
-        rest = join(htsno_parts[3:end])
+        _8_10 = join(htsno_parts[3:end])
         if haskey(r, :description) && !isempty(r.description)
-            txt = get(h6_id_text, H6_id, "")
+            txt = get(h6_dict, H6_id, "")
             if contains(lowercase(txt), lowercase(r.description))
                 @warn "already in $H6_id text: $(r.description)"
                 continue
             end
-            @show h6_id_text[H6_id] = "$txt; $rest - $(r.description)"
+            h6_dict[H6_id] = "$txt; $_8_10 - $(r.description)"
         end
     end
-    h6_id_text
-end
-export rollup
-
-function dct_to_md(;d::Dict, filepath::AbstractString)
-    kees = sort(collect(keys(d)))
-    open(filepath, "w") do io
-        for k in kees
-            println(io, "## H6 Code $k")
-            println(io, d[k])
-            println(io, "")
+    if outputmd
+        md_filepath = joinpath(outdir, "H6_rollup.md")
+        open(md_filepath, "w") do io
+            for k in sort(collect(keys(h6_dict)))
+                println(io, "## H6 Code $k")
+                println(io, h6_dict[k])
+                println(io, "")
+            end
         end
     end
+    h6_dict
 end
-export dct_to_md
-
-function rollupdown(;h6_filepath::AbstractString="./H6.json",
-                    htsdata_filepath::AbstractString="./htsdata.json",
-                    md_filepath::AbstractString="./H6_rollup.md")
-    h6_json3 = load_HX(dirpath=dirname(h6_filepath), filename=basename(h6_filepath))
-    h6_id_txt = h6_id_text(h6_json3)
-    htsdata_json3 = JSON3.read(htsdata_filepath)
-    h6_id_txt = rollup(h6_id_text=h6_id_txt, htsdata_json3=htsdata_json3)
-    dct_to_md(;d=h6_id_txt, filepath=md_filepath)
-end
-export rollupdown
+export rollupH6
 
 end # module HStools
